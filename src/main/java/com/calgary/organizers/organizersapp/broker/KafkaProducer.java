5d162192bb6dab98e63a333bd2fe3163bb671c3a
Emmanuel Adakole Audu <kole@Emmanuels-MacBook-Pro-2.local>
2024-09-28 12:52:06 -0600

    new changes

---
+++ src/main/java/com/calgary/organizers/organizersapp/broker/KafkaProducer.java
@@ -0,0 +1,13 @@
+package com.calgary.organizers.organizersapp.broker;
+
+import java.util.function.Supplier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaProducer implements Supplier<String> {
+
+    @Override
+    public String get() {
+        return "kakfa_producer";
+    }
+}
