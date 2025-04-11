bc0069885b3bc35635e1a6d8ce56056c82874444
emmanuel96 <kole.audu@gmail.com>
2025-04-11 14:31:28 -0600

    added the day to the mobile view

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -270,4 +270,10 @@ export default class HomeComponent implements OnInit, OnDestroy {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return this.groupedEvents;
   }
+
+  isSameDay(date1: string | Date, date2: string | Date): boolean {
+    const d1 = new Date(date1);
+    const d2 = new Date(date2);
+    return d1.getFullYear() === d2.getFullYear() && d1.getMonth() === d2.getMonth() && d1.getDate() === d2.getDate();
+  }
 }
