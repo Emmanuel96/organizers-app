cb0f7972d57e51b64e4931ca8c2432a6536f54d2
Petro Zinchenko <petro.zinchenko636@gmail.com>
2025-08-08 18:58:48 -0600

    Fix hardcode validators

--- src/main/java/com/calgary/organizers/organizersapp/utils/meetup/MeetupCityValidator.java
+++ src/main/java/com/calgary/organizers/organizersapp/utils/meetup/MeetupCityValidator.java
@@ -1,7 +1,9 @@
 package com.calgary.organizers.organizersapp.utils.meetup;
 
+import com.calgary.organizers.organizersapp.config.DomainConfig;
 import com.calgary.organizers.organizersapp.service.eventsource.meetup.InvalidGroupParameterException;
 import com.fasterxml.jackson.databind.JsonNode;
+import java.util.Objects;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
@@ -9,10 +11,16 @@ import org.springframework.stereotype.Component;
 @ConditionalOnProperty(prefix = "domain.meetup", name = "city")
 public class MeetupCityValidator implements MeetupDomainValidator {
 
+    private final String city;
+
+    public MeetupCityValidator(DomainConfig props) {
+        this.city = props.getMeetup().getCity();
+    }
+
     @Override
     public void validate(JsonNode rootNode) {
-        var city = rootNode.at("/data/groupByUrlname/city").asText();
-        if (!city.equals("Calgary")) {
+        var requestCity = rootNode.at("/data/groupByUrlname/city").asText();
+        if (!Objects.equals(requestCity, city)) {
             throw new InvalidGroupParameterException("Group from wrong city provided. Provided '%s'".formatted(city));
         }
     }
--- src/main/java/com/calgary/organizers/organizersapp/utils/meetup/MeetupTopicCategoryValidator.java
+++ src/main/java/com/calgary/organizers/organizersapp/utils/meetup/MeetupTopicCategoryValidator.java
@@ -1,7 +1,9 @@
 package com.calgary.organizers.organizersapp.utils.meetup;
 
+import com.calgary.organizers.organizersapp.config.DomainConfig;
 import com.calgary.organizers.organizersapp.service.eventsource.meetup.InvalidGroupParameterException;
 import com.fasterxml.jackson.databind.JsonNode;
+import java.util.Objects;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
@@ -9,11 +11,19 @@ import org.springframework.stereotype.Component;
 @ConditionalOnProperty(prefix = "domain.meetup", name = "required-topic-category")
 public class MeetupTopicCategoryValidator implements MeetupDomainValidator {
 
+    private final String topicCategory;
+
+    public MeetupTopicCategoryValidator(DomainConfig props) {
+        this.topicCategory = props.getMeetup().getRequiredTopicCategory();
+    }
+
     @Override
     public void validate(JsonNode rootNode) {
-        var topicCategory = rootNode.at("/data/groupByUrlname/topicCategory/name").asText();
-        if (!topicCategory.equals("Technology")) {
-            throw new InvalidGroupParameterException("Group from wrong topic category provided. Provided '%s'".formatted(topicCategory));
+        var requestTopicCategory = rootNode.at("/data/groupByUrlname/topicCategory/name").asText();
+        if (!Objects.equals(requestTopicCategory, topicCategory)) {
+            throw new InvalidGroupParameterException(
+                "Group from wrong topic category provided. Provided '%s'".formatted(requestTopicCategory)
+            );
         }
     }
 }
